f0b1b4f5da6f3048882d69b95bedaf0a7286b089
faqihyugos <faqihyugos@gmail.com>
2026-05-18 15:35:02 +0700

    refactor: improve input handling and add default values for nama and kota

--- Biodata.cs
+++ Biodata.cs
@@ -1,12 +1,19 @@
 Console.Write("Nama : ");
-string nama = Console.ReadLine();
-Console.Write("Umur : ");
-int umur = Convert.ToInt32(Console.ReadLine());
+string nama = Console.ReadLine() ?? string.Empty;
 Console.Write("Kota : ");
-string kota = Console.ReadLine();
+string kota = Console.ReadLine() ?? string.Empty;
+try
+{
+    Console.Write("Umur : ");
+    int umur = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine();
-Console.WriteLine("======Biodata=====");
-Console.WriteLine($"Nama: {nama}");
-Console.WriteLine($"Umur: {umur}");
-Console.WriteLine($"Kota: {kota}");
+    Console.WriteLine();
+    Console.WriteLine("======Biodata=====");
+    Console.WriteLine($"Nama: {nama}");
+    Console.WriteLine($"Kota: {kota}"); 
+    Console.WriteLine($"Umur: {umur}");
+}
+catch
+{
+    Console.WriteLine("Input harus angka");
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# BelajarCSharp
+
+Saya belajar bersama Dasar C# dengan pak wahyu nurhidayat
